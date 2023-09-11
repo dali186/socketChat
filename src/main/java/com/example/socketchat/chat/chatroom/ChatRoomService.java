c43719373a57d771805de16a17f235c2575cbf1f
JOOWON <wer07032@naver.com>
2023-09-12 02:26:09 +0900

    feat: implement logic that handle duplicate chatroom

--- src/main/java/com/example/socketchat/chat/chatroom/ChatRoomService.java
+++ src/main/java/com/example/socketchat/chat/chatroom/ChatRoomService.java
@@ -14,8 +14,13 @@ public class ChatRoomService {
 
     @Transactional
     public String openChatRoom(ChatRoom chatRoom) {
-        ChatRoom savedRoom = chatRoomRepository.save(chatRoom);
-        return savedRoom.getId();
+        //중복처리 검증 로직
+        ChatRoom duplicatedRoom = chatRoomRepository.findDuplicatedRoom(chatRoom.getMemberList().get(0), chatRoom.getMemberList().get(1));
+        if (duplicatedRoom == null) {
+            ChatRoom savedRoom = chatRoomRepository.save(chatRoom);
+            return savedRoom.getId();
+        }
+        return duplicatedRoom.getId();
     }
 
     @Transactional
@@ -28,5 +33,10 @@ public class ChatRoomService {
     public List<ChatRoom> findMemberPersonalChatRooms(Long memberId) {
         List<ChatRoom> chatRooms = chatRoomRepository.findPersonalRoomById(memberId);
         return chatRooms;
+    }
+
+    @Transactional
+    public ChatRoom findChatRoomById(String roomId) {
+        return chatRoomRepository.findByRoomById(roomId);
     }
 }
