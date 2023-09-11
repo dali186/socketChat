fa50a546845b5ed80d2064e0ce6036b5aa0501c5
JOOWON <wer07032@naver.com>
2023-09-12 02:22:23 +0900

    refactor: Move enterChat method to ChatRoomController

--- src/main/java/com/example/socketchat/chat/ChatController.java
+++ src/main/java/com/example/socketchat/chat/ChatController.java
@@ -2,20 +2,14 @@ package com.example.socketchat.chat;
 
 import com.example.socketchat.chat.chatmessage.ChatMessage;
 import com.example.socketchat.chat.chatmessage.ChatMessageRepository;
-import com.example.socketchat.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -36,14 +30,5 @@ public class ChatController {
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
-    }
-
-    @GetMapping("/chat/room/{roomId}")
-    public String eneterChat(@PathVariable String roomId, @AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-        List<ChatMessage> messageList = chatMessageRepository.mFindByRoomId(roomId);
-        model.addAttribute("memberId", userDetails.getUserId());
-        model.addAttribute("messageList", messageList);
-
-        return "chat";
     }
 }
