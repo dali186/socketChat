4eb2ed5b878e942a25b372520b361db1bc2c6fc1
JOOWON <wer07032@naver.com>
2023-09-12 02:22:49 +0900

    feat: implement findByRoomId

--- src/main/java/com/example/socketchat/chat/chatroom/ChatRoomRepository.java
+++ src/main/java/com/example/socketchat/chat/chatroom/ChatRoomRepository.java
@@ -7,12 +7,15 @@ import java.util.List;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
 
-    @Query("{memberList: ?0}")
-    List<ChatRoom> mFindRoomById(Long memberId);
+    @Query("{$and: [{memberList: ?0}, {memberList: ?1}]}")
+    ChatRoom findDuplicatedRoom(Long memberId, Long opId);
 
     @Query("{memberList: ?0, type: 'PERSONAL'}")
     List<ChatRoom> findPersonalRoomById(Long memberId);
 
     @Query("{memberList: ?0}")
     List<ChatRoom> findMemberRoomById(Long memberId);
+
+    @Query("{_id: ?0}")
+    ChatRoom findByRoomById(String roomId);
 }
